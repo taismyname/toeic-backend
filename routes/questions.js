32fc7315fcb5a36bdfee9673a0f0223b6da3924e
Tai Nguyen <112757316+taismyname@users.noreply.github.com>
2025-08-18 14:47:19 +0700

    Create questions.js

---
+++ routes/questions.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const { getQuestions } = require('../controllers/questionController');
+const router = express.Router();
+
+router.get('/', getQuestions);
+
+module.exports = router;
